2cb7ba5537d1c9ffcf0300d9ddd76812346cbf08
Simon Clément <simon.clement@ensimag.fr>
2018-04-23 23:20:32 +0200

    ajout du pterosaur et modification du model dino

--- src/dino.py
+++ src/dino.py
@@ -5,6 +5,7 @@ Python OpenGL dinosaurus : sweet and flying with geysers
 
 import glfw                         # lean window system wrapper for OpenGL
 from src.transform import rotate, translate
+import math
 
 class Dino:
     """This class should make the dino fly :3 """
@@ -35,12 +36,41 @@ class Dino:
         transform = rotate(axis=(0,1,0), angle=self.angle) @ translate(0,self.pos_z,0)
         model = model @ transform
         self.node_dino.draw(projection, view, model,
-                            time=(self.time_offset - self.offset_animation)*3, **param)
+                            time=(self.time_offset - self.offset_animation), **param)
 
     def new_geyser(self, charge):
         """ fait voler le dino si il est au sol..."""
         # en vrai faudra faire une fonction qui ajoute de la vitesse
         # en fonction de sa position
         self.vitesse_z += charge / (0.1 * self.pos_z + 1)
-        self.offset_animation = glfw.get_time()
-        # TODO reset l'animation du dino
+        if self.pos_z == 0:
+            self.offset_animation = glfw.get_time()
+
+class Ptero:
+    """This class should make the ptero fly :3 """
+    def __init__(self, node_dino):
+        self.node_dino = node_dino
+        self.angle = 0
+        self.distance = 40
+        self.hauteur = 20
+        self.time_offset = glfw.get_time()
+
+    def draw(self, projection, view, model, **param):
+        """just draw the node, passing all arguments"""
+        # --- recuperation du temps passe
+        newt = glfw.get_time()
+        dt = newt - self.time_offset
+        self.time_offset = newt
+
+        # --- actualisation des donnees
+        self.angle += 20 * dt
+        time_in_animation = self.time_offset%5
+        real_height = self.hauteur + 7*math.cos(-0.5+time_in_animation * 2*3.1415/5)
+        #(3 - time_in_animation) 
+        #* (1 - 5/2 * (time_in_animation >= 3))*3
+
+        transform = rotate(axis=(0,1,0), angle=self.angle) @ translate(self.distance,real_height,0)
+        model = model @ transform
+        self.node_dino.draw(projection, view, model,
+                            time=time_in_animation, **param)
+
