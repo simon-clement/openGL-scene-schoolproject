1ba3b6623f40447cd5391c72f1bbdffe8f285db2
Julie Lauquin <Julie.Lauquin@grenoble-inp.org>
2018-04-30 12:59:52 +0200

    texture sur skybox

--- src/shader.py
+++ src/shader.py
@@ -276,9 +276,12 @@ void main() {
     vec4 position3D = modelviewprojection * vec4(position2, 1);
     gl_Position = position3D;
     // rayon courant = sqrt(x**2 + y**2)
-    // angle courant =
-    // distance sur la texture = angle/total * 2pi*r
-    fragTexCoord = vec2(position3D[0], - position3D[2]);
+    // angle courant = atan(x/y)
+    // distance sur la texture (longitude)= (angle/(2*pi)) * 2pi*r
+    float rayon = sqrt(pow(position[0],2) + pow(position[2],2));
+    float latitude =  - (position[1]/2 - 0.5);
+    float longitude = atan(position[2]/(position[0]+1)) *2*rayon;
+    fragTexCoord = vec2(longitude, latitude);
 }"""
 
 SKYBOX_FRAG = """#version 330 core
