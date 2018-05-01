5cf65ef960204c08d62f7cc5b3e3bce28cea1b3e
Paul Ghib <paul.bourgeois@ensimag.fr>
2018-05-01 18:34:12 +0200

    poussage

--- src/arbre.py
+++ src/arbre.py
@@ -8,18 +8,35 @@ from src.transform import rotate, translate, scale
 from src.node import Node
 from src.cylindre import Cylindre
 import math
+from random import random
+
+def faire_pousser_sur(tronc, branches_restantes):
+    branches_restantes -= 1
+    hauteur = tronc.hauteur / 3.
+
+    for i in range(2):
+        transform = (translate(0, 0, 0)     # orientation des branches
+                @ scale(hauteur)            # diminuer la taille de la branche
+                @ translate(0, 0, 0))       # pour placer au sommet du tronc
+        branche = Node(transform=transform, color=(.65, .40, .15))
+        tronc.add(branche)
+        
+        if branches_restantes < 0:
+            faire_pousser_sur(branche, branches_restantes)
+    
 
 class Arbre:
     """Class of tree"""
     def __init__(self):
+        self.hauteur = 10
+
         transform = (translate(0, 0, 0)
-                @ scale(10)
+                @ scale(self.hauteur)
                 @ translate(0, 0, 0))
         tronc = Node(name='arbre', transform=transform, color=(167, 103, 38))
         tronc.add(Cylindre())
 
-        branche = Node(name='branche', transform=transform, color=(.65, .40, .15))
-        tronc.add(branche)
+        faire_pousser_sur(tronc, 1)
 
     def draw(self, projection, view, model, **param):
         """just draw the node, passing all arguments"""
